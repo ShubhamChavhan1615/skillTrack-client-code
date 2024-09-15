70cd8831f11366673546f2ee8a177905b9535ee5
Shubham Chavhan <shubhamchavhan908@gmail.com>
2024-09-15 11:08:01 +0530

    env file change

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,13 +1,13 @@
-export const sendOtpApi = `${import.meta.env.SERVER_API}/api/valid/email`
-export const loginApi = `${import.meta.env.SERVER_API}/api/user/login`
-export const signUpApi =  `${import.meta.env.SERVER_API}/api/user/signUp`
-export const getUserProfileApi = `${import.meta.env.SERVER_API}/api/user/profile`
-export const getCourses = `${import.meta.env.SERVER_API}/api/all/courses`
-export const addCourse =  `${import.meta.env.SERVER_API}/instructor/api/create/course`
-export const getAllInstructors = `${import.meta.env.SERVER_API}/api/all/instructors`
-export const addQuizeApi = `${import.meta.env.SERVER_API}/quize/api/create/quiz`
-export const AIChatApi = `${import.meta.env.SERVER_API}/api/ai/chat`
-export const createCourseApi = `${import.meta.env.SERVER_API}/instructor/api/create/course`
-export const editCourseApi = `${import.meta.env.SERVER_API}/instructor/api/edit/course`
+export const sendOtpApi = `${import.meta.env.VITE_SERVER_API}/api/valid/email`
+export const loginApi = `${import.meta.env.VITE_SERVER_API}/api/user/login`
+export const signUpApi =  `${import.meta.env.VITE_SERVER_API}/api/user/signUp`
+export const getUserProfileApi = `${import.meta.env.VITE_SERVER_API}/api/user/profile`
+export const getCourses = `${import.meta.env.VITE_SERVER_API}/api/all/courses`
+export const addCourse =  `${import.meta.env.VITE_SERVER_API}/instructor/api/create/course`
+export const getAllInstructors = `${import.meta.env.VITE_SERVER_API}/api/all/instructors`
+export const addQuizeApi = `${import.meta.env.VITE_SERVER_API}/quize/api/create/quiz`
+export const AIChatApi = `${import.meta.env.VITE_SERVER_API}/api/ai/chat`
+export const createCourseApi = `${import.meta.env.VITE_SERVER_API}/instructor/api/create/course`
+export const editCourseApi = `${import.meta.env.VITE_SERVER_API}/instructor/api/edit/course`
 // http://localhost:4000/instructor/api/delete/course
-export const deleteCourseApi = `${import.meta.env.SERVER_API}/instructor/api/delete/course`
+export const deleteCourseApi = `${import.meta.env.VITE_SERVER_API}/instructor/api/delete/course`
