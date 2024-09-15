e9ac5031855ae8203f8ff6566dd2a9b8306489a1
Shubham Chavhan <shubhamchavhan908@gmail.com>
2024-09-15 10:38:26 +0530

    few changes

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,7 @@
-export const sendOtpApi = `http://localhost:4000/api/valid/email`
-export const loginApi = `http://localhost:4000/api/user/login`
-export const signUpApi =  `http://localhost:4000/api/user/signUp`
-export const getUserProfileApi = `http://localhost:4000/api/user/profile`
-export const getCourses = `http://localhost:4000/api/all/courses`
-export const addCourse =  `http://localhost:4000/instructor/api/create/course`
-export const getAllInstructors = `http://localhost:4000/api/all/instructors`
+export const sendOtpApi = `${import.meta.env.SERVER_API}/api/valid/email`
+export const loginApi = `${import.meta.env.SERVER_API}/api/user/login`
+export const signUpApi =  `${import.meta.env.SERVER_API}/api/user/signUp`
+export const getUserProfileApi = `${import.meta.env.SERVER_API}/api/user/profile`
+export const getCourses = `${import.meta.env.SERVER_API}/api/all/courses`
+export const addCourse =  `${import.meta.env.SERVER_API}/instructor/api/create/course`
+export const getAllInstructors = `${import.meta.env.SERVER_API}/api/all/instructors`
